5549277a0a17551f0b75df349f18f12c9c070f34
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-11-16 23:56:33 +0300

    Added tests generator and its configuration

---
+++ ConsoleApplication/Entities/TestsGeneratorConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApplication.Entities
+{
+    public class TestsGeneratorConfiguration
+    {
+        public TestsGeneratorConfiguration(
+            string destinationDirectory,
+            int maxCountOfParallelFilesRead,
+            int maxCountOfParallelTestsGenerationTasks,
+            int maxCountOfParallelFilesWrite)
+        {
+            DestinationDirectory = destinationDirectory;
+            MaxCountOfParallelFilesRead = maxCountOfParallelFilesRead;
+            MaxCountOfParallelTestsGenerationTasks = maxCountOfParallelTestsGenerationTasks;
+            MaxCountOfParallelFilesWrite = maxCountOfParallelFilesWrite;
+        }
+
+        public string DestinationDirectory { get; }
+        public int MaxCountOfParallelFilesRead { get; }
+        public int MaxCountOfParallelTestsGenerationTasks { get; }
+        public int MaxCountOfParallelFilesWrite { get; }
+    }
+}
---
+++ ConsoleApplication/Services/ITestsGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.Services
+{
+    public interface ITestsGenerator
+    {
+        Task Generate(IEnumerable<string> filesPaths);
+    }
+}
