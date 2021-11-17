dcaa4e651e2a6931e67093743cd586633c236c1f
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-11-17 20:06:11 +0300

    Added tests creator and test file class

---
+++ Core/Entities/TestFile.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Entities
+{
+    public class TestFile
+    {
+        public TestFile(string filename, string contents)
+        {
+            Filename = filename;
+            Contents = contents;
+        }
+
+        public string Filename { get; }
+        public string Contents { get; }
+    }
+}
---
+++ Core/Services/ITestsCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Core.Entities;
+
+namespace Core.Services
+{
+    public interface ITestsCreator
+    {
+        IEnumerable<TestFile> Create(string fileContents);
+    }
+}
